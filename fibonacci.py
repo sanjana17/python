5e493b843f0f210f7d9e17ee6b6524e8006895ba
Pranith Macha <pranith@machaiswho.im>
2017-02-15 21:24:00 -0600

    modified fibonacci to loop through a list

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,16 @@
-i = int(raw_input("enter a number "));
+
 def fibonacci(n):
-	if n==0 or n==1:
+	if n == 0 or n == 1:
 	   return n;
 	else:
 		return fibonacci(n-1)+fibonacci(n-2);
 
-a = fibonacci(i);
-print a;
+if __name__ == "__main__":
+	i = int(input("enter a number "))
+	print(fibonacci(i))
+	
+	# loop through range of numbers 
+	# range(n) generates list of numbers starting from 0 to n-1 
+	# eg: range(5) = [0, 1, 2, 3, 4]
+	for n in range(5):
+		print(n, fibonacci(n))
